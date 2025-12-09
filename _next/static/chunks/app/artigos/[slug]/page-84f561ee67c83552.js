27bc7c6fc83eafa9c22016be1b330404273f013f
ozairx <ozairx@users.noreply.github.com>
2025-12-09 16:11:06 +0000

    deploy: ozairx/doutorluisbraga.com@efb0c3f966b2f7c797efe9fc99a445fbc221325f

---
+++ _next/static/chunks/app/artigos/[slug]/page-84f561ee67c83552.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[346],{8038:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3279,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[279,230,569,358],()=>s(8038)),_N_E=e.O()}]);
---
+++ _next/static/xTUJ7RSfD0oWPJBZoLeUm/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fartigos\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
